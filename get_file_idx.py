0afb4237cafd065839133cef945a90c5c60dcfce
GTEC Dev <dev@gomezsystems.com>
2026-05-02 22:50:56 -0700

    Repository maintanence

--- get_file_idx.py
+++
@@ -1,6 +0,0 @@
-import os
-files = [f for f in os.listdir("telemetry") if f.endswith(".ibt")]
-for i, f in enumerate(files):
-    if "porsche992rgt3_indianapolis" in f:
-        print("FOUND", f, "at index", i+1) # wait, it's sorted by display name.
-
